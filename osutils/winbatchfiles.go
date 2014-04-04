6b1eede67390abc097f717cdcb7bc9c88bd8a880
Francois <francoishill11@gmail.com>
2014-04-04 10:38:53 +0200

    Rather make it a batch file that always waits, and returns the output.

--- osutils/winbatchfiles.go
+++ osutils/winbatchfiles.go
@@ -9,39 +9,47 @@ import (
 	"time"
 )
 
-func RunTempWindowsBatchFile(commandsContent string, mustWait bool, tempDir string) error {
+var runningCount int = 0
+
+func HasRunningBatchFiles() bool {
+	return runningCount > 0
+}
+
+func RunTempBatchFileAndGetOutput(commandsContent string, tempDir string) ([]byte, error) {
 	var err error
 	tempBatchFilesDir := path.Join(tempDir, "_TempBatchFiles")
 	if !PathExists(tempBatchFilesDir) {
 		err = os.MkdirAll(tempBatchFilesDir, 0600)
 		if err != nil {
-			return err
+			return nil, err
 		}
 	}
 	now := time.Now()
-	tmpBatchFile := path.Join(tempBatchFilesDir, fmt.Sprintf("%04d-%02d-%02d %02d_%02d_%02d.%09d.bat", now.Year(), now.Month(), now.Day(), now.Hour(), now.Minute(), now.Second(), now.Nanosecond()))
+
+	fileNameOnly := fmt.Sprintf("%04d-%02d-%02d %02d_%02d_%02d.%09d.bat", now.Year(), now.Month(), now.Day(), now.Hour(), now.Minute(), now.Second(), now.Nanosecond())
+
+	tmpBaseBatchFilePath := path.Join(tempBatchFilesDir, "1 - "+fileNameOnly)
+	tmpBatchFileContentPath := path.Join(tempBatchFilesDir, "2 - "+fileNameOnly)
+
+	baseBatchContent := "@echo off\r\n"
+	//baseBatchContent += "start \"\" /wait \"" + tmpBatchFileContentPath + "\""
+	baseBatchContent += "call \"" + tmpBatchFileContentPath + "\""
+	baseBatchContent += "\r\n"
+	err = ioutil.WriteFile(tmpBaseBatchFilePath, []byte(baseBatchContent), 0600)
+	if err != nil {
+		return nil, err
+	}
+	defer os.Remove(tmpBaseBatchFilePath)
 
 	batchFileContent := "@echo off\r\n"
-	batchFileContent += "start \"\" /wait "
 	batchFileContent += commandsContent
-	err = ioutil.WriteFile(tmpBatchFile, []byte(batchFileContent), 0600)
+	batchFileContent += "\r\n"
+	err = ioutil.WriteFile(tmpBatchFileContentPath, []byte(batchFileContent), 0600)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	defer os.Remove(tmpBatchFile)
+	defer os.Remove(tmpBatchFileContentPath)
 
-	cmd := exec.Command(tmpBatchFile)
-	err = cmd.Start()
-	if err != nil {
-		return err
-	}
-
-	if mustWait {
-		err = cmd.Wait()
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
+	cmd := exec.Command(tmpBaseBatchFilePath)
+	return cmd.CombinedOutput()
 }
